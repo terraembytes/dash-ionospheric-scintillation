36f098b5598d6dfe147070f1296f76f0c1d91a35
oniyudi <onimaru.yudi@gmail.com>
2025-09-08 15:24:57 -0300

    inicando a interface inicial e alterando a aba do navegador

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css'
+import imglogo from './assets/img/logo.png'
 
 function App() {
 
   return (
-    <div>
+    <div className='overflow-hidden'>
       {/*DIV DO HEADER*/}
-      <div className='flex justify-between'>
+      <div className='flex justify-between items-center p-2 titles-css text-amber-50 rounded-2xl shadow-lg'>
+        <div><img src={imglogo} alt="logo do projeto" width={110} height={100}/></div>
         <div>Hello World</div>
         <div>Hello World</div>
-        <div>Hello World</div>
+      </div>
+
+      {/*DIV DA AREA DOS DASHBOARDS*/}
+      <div className='bg-gray-100 w-full h-screen mt-6 rounded-2xl p-2 shadow-lg'>
+        Hello World
       </div>
     </div>
   )
