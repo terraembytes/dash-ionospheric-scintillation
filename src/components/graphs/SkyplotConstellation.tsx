4308eb548b69729f406e8686dbdf952cb7397d51
oniyudi <onimaru.yudi@gmail.com>
2026-01-16 18:28:17 -0300

    reformulando as rotas e adaptacoes necessarias

--- src/components/graphs/SkyplotConstellation.tsx
+++ src/components/graphs/SkyplotConstellation.tsx
@@ -2,8 +2,13 @@ import Plot from 'react-plotly.js'
 import type { DataGeral } from '../../models/DataGeral'
 
 interface Props {
-  data: DataGeral[],
+  data: DataGeralSkyplot[],
   title: string
+}
+
+interface DataGeralSkyplot extends DataGeral {
+  Azimute: string
+  Intensity: string
 }
 
 function SkyplotConstellation({ data = [], title }: Props) {
@@ -11,14 +16,42 @@ function SkyplotConstellation({ data = [], title }: Props) {
     <Plot
       data={[
         {
-          x: data.map((dados: DataGeral) => { return dados.Date }),
-          y: data.map((dados: DataGeral) => { return dados.S4 }),
           mode: 'markers',
-          type: 'scatter',
-          marker: { color: data.map((dados: DataGeral) => { return dados.Svid }) }
+          type: 'scatterpolar',
+          r: data.map(p => Number(p.Elevation)),
+          theta: data.map(p => Number(p.Azimute)),
+          marker: { 
+            color: data.map(p => p.Intensity),
+            size: 10
+          }
         }
       ]}
-      layout={{ title: { text: title }, autosize: true }}
+      layout={{ 
+        title: { text: title }, 
+        autosize: true,
+        polar: {
+          domain: { y: [0, 0.9] },
+          radialaxis: {
+            range: [90, 0],
+            tickvals: [90, 75, 60, 45, 30, 15, 0],
+            ticktext: ['90°', '75°', '60°', '45°', '30°', '15°', '0°'],
+            angle: 90,
+            showline: true,
+            dtick: 15,
+            showticklabels: true,
+            tickfont: { color: '#000000', size: 12 },
+            gridcolor: '#e0e0e0',
+            linecolor: '#000000'
+          },
+          angularaxis: {
+            direction: "clockwise",
+            rotation: 90,
+            tickfont: { color: '#000000', size: 12 },
+            gridcolor: '#e0e0e0',
+            linecolor: '#000000'
+          },
+        },
+      }}
       useResizeHandler={true}
       className="w-full h-full"
     />
