c294e91399eeea917a6691d5a50ca6e5309a9fc4
oniyudi <onimaru.yudi@gmail.com>
2026-02-13 16:37:26 -0300

    feat: add componente skyplot polygon

--- src/components/graphs/SkyplotConstellation.tsx
+++ src/components/graphs/SkyplotConstellation.tsx
@@ -1,70 +1,37 @@
 import Plot from 'react-plotly.js'
-import type { DataGeral } from '../../models/DataGeral'
-import { useMemo } from 'react'
+import type { DataSkyplot } from '../../models/DataSkyplot'
+
+interface SkyplotPolygon {
+  polygons: [],
+  points: [],
+}
 
 interface Props {
-  data: DataGeralSkyplot[],
+  data: DataSkyplot[],
+  polygons: SkyplotPolygon
   titles: string
 }
 
-interface DataGeralSkyplot extends DataGeral {
-  Azimute: string
-  Intensity: string
-}
+function SkyplotConstellation({ data = [], polygons, titles }: Props) {
+  const list_x = polygons.points.map((x) => {
+      return x[0]
+  })
 
-function SkyplotConstellation({ data = [], titles }: Props) {
-  const textSvid = data.map(p => p.Svid.toString());
+  const list_y = polygons.points.map((y) => {
+    return y[1]
+  })
 
-  // criando a trace segundaria para o grafico
-  const secondTrace = useMemo(() => {
-    // filtrando os pontos com valor S4 maior ou igual a 0.6
-    const highS4Points = data.filter(d => Number(d.S4) >= 0.60)
-
-    //converter para o cartesiano
-    //Centro = 0 de raio visual (90 elevação)
-    //Borda = 90 de raio visual (0 elevação)
-    const cartesianPoints = highS4Points.map(p => {
-      const r_visual = 90 - p.Elevation //quao longe o ponto esta do centro
-      const theta_rad = (Number(p.Azimute) - 90) * (Math.PI /  180) //padrão 0 é leste
-
-      return {
-        ...p,
-        x: r_visual * Math.cos(theta_rad),
-        y: r_visual * Math.sin(theta_rad)
-      }
-    })
-
-    // Threshold: Distância visual (em graus de elevação)
-    const DISTANCE_THRESHOLD = 15
-
-    const size = cartesianPoints.map((p1, _, arr) => {
-      //conta quantos pontos p2 estão perto de p1
-      const neighbors = arr.reduce((count, p2) => {
-        const dist = Math.sqrt(Math.pow(p1.x - p2.x, 2) + (Math.pow(p1.y - p2.y, 2)))
-        return dist <= DISTANCE_THRESHOLD ? count + 1 : count
-      }, 0)
-
-      // Fórmula de tamanho: Base + (Fator * Vizinhos)
-      return 15 + (neighbors * 5)
-    })
-
-    return {
-      type: 'scatterpolar',
-      mode: 'markers',
-      r: highS4Points.map(d => d.Elevation), // Plotly usa a elevação original se o eixo estiver configurado corretamente
-      theta: highS4Points.map(d => d.Azimute),
-      marker: {
-        size: size,
-        color: 'rgba(255, 0, 0, 0.1)', // Vermelho com transparência
-        line: {
-          color: 'rgba(255, 0, 0, 0.6)',
-          width: 1
-        }
-      },
-      hoverinfo: 'none', // Remove hover desse trace pra não atrapalhar
-      name: 'Área de Cintilação'
-    } as Plotly.Data;
-  }, [data])
+  //construindo a trace dos poligonos
+  const polygonTrace: Plotly.Data = {
+    x: list_x,
+    y: list_y,
+    mode: 'lines',
+    fill: 'toself',
+    type: 'scatter',
+    fillcolor: 'rgba(255, 0, 0, 0.2)', // Semi-transparent red
+    line: { color: 'red' },
+    //hoveron: 'points+fills' // Show hover info on the shape itself
+  }
 
   return (
     <Plot
@@ -74,7 +41,7 @@ function SkyplotConstellation({ data = [], titles }: Props) {
           type: 'scatterpolar',
           r: data.map(p => Number(p.Elevation)),
           theta: data.map(p => Number(p.Azimute)),
-          text: textSvid,
+          //text: textSvid,
           textposition: 'middle left',
           textfont: { size: 11, color: '#000000' },
           cliponaxis: false,
@@ -99,7 +66,7 @@ function SkyplotConstellation({ data = [], titles }: Props) {
             //line: { width: 1, color: '#ffffff' }
           }
         },
-        secondTrace
+        polygonTrace
       ]}
       layout={{ 
         title: { text: titles }, 
